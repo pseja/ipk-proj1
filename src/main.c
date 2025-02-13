f21f08b5fed006164ee0c2185df38ff6fb417ed4
Lukas Pseja <luk4sps3j4@gmail.com>
2025-02-13 15:55:48 +0100

    argument parsing and validation should be done TODO: testing

--- src/main.c
+++ src/main.c
@@ -14,14 +14,6 @@ typedef uint16_t u_short;
 typedef uint8_t u_char;
 #include <pcap.h>
 
-#include <arpa/inet.h>
-#include <errno.h>
-#include <netdb.h>
-#include <netinet/ip_icmp.h>
-#include <netinet/tcp.h>
-#include <netinet/udp.h>
-#include <unistd.h>
-
 #define RED "\e[0;31m"
 #define YEL "\e[0;33m"
 #define BLK "\e[0;30m"
@@ -137,132 +129,24 @@ void printNetworkInterfaces()
     pcap_freealldevs(interfaces);
 }
 
-typedef struct Options
-{
-    char *interface;
-    char *udp_ports;
-    char *tcp_ports;
-    int timeout;
-    char *target;
-} Options;
-
-Options parse_options(int argc, char **argv)
-{
-    if (argc == 1)
-    {
-        printNetworkInterfaces();
-        exit(EXIT_SUCCESS);
-    }
-
-    Options opts = {.interface = NULL, .udp_ports = NULL, .tcp_ports = NULL, .timeout = 5000, .target = NULL};
-
-    int opt;
-    const char *short_options = "i::u:t:w:";
-    static struct option long_options[] = {{"interface", optional_argument, 0, 'i'},
-                                           {"pu", required_argument, 0, 'u'},
-                                           {"pt", required_argument, 0, 't'},
-                                           {"timeout", required_argument, 0, 'w'},
-                                           {0, 0, 0, 0}};
-
-    while ((opt = getopt_long(argc, argv, short_options, long_options, NULL)) != -1)
-    {
-        switch (opt)
-        {
-        case 'i':
-            if (opts.interface != NULL)
-            {
-                fprintf(stderr, RED "[Error] " RES "Interface is already specified.\n");
-                exit(EXIT_FAILURE);
-            }
-
-            if (optarg)
-            {
-                opts.interface = optarg;
-            }
-            else if (optind < argc && argv[optind][0] != '-')
-            {
-                opts.interface = argv[optind++];
-            }
-            else
-            {
-                printNetworkInterfaces();
-                exit(EXIT_SUCCESS);
-            }
-
-            if (!isInterfaceValid(opts.interface))
-            {
-
-                fprintf(stderr, RED "[Error] " RES "Interface is not valid\n");
-                exit(EXIT_FAILURE);
-            }
-            break;
-        case 'u':
-            // TODO: parse -u 22 or -u 22,23,24 or -u 1-23
-            if (opts.udp_ports != NULL)
-            {
-                fprintf(stderr, RED "[Error] " RES "UDP port-ranges are already specified.\n");
-                exit(EXIT_FAILURE);
-            }
-            opts.udp_ports = optarg;
-            break;
-        case 't':
-            // TODO: parse -t 22 or -t 22,23,24 or -t 1-23
-            if (opts.tcp_ports != NULL)
-            {
-                fprintf(stderr, RED "[Error] " RES "TCP port-ranges are already specified.\n");
-                exit(EXIT_FAILURE);
-            }
-            opts.tcp_ports = optarg;
-            break;
-        case 'w': {
-            opts.timeout = atoi(optarg);
-            if (opts.timeout <= 0)
-            {
-                fprintf(stderr, RED "[Error] " RES "Invalid timeout value.\n");
-                exit(EXIT_FAILURE);
-            }
-            break;
-        }
-        case '?': {
-            exit(EXIT_FAILURE);
-        }
-        default:
-            fprintf(stderr, RED "[Error] " RES "Failed unexpectedly.\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    if (optind == argc - 1)
-    {
-        opts.target = argv[optind];
-    }
-    else
-    {
-        fprintf(stderr, RED "[Error] " RES "Exactly one domain-name or IP-address must be provided.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    return opts;
-}
-
-bool isValidPortNumber(int port_number)
-{
-    return port_number >= 1 && port_number <= 65535;
-}
-
 int regmatch(const char *pattern, const char *input)
 {
     regex_t regex;
     int result = regcomp(&regex, pattern, REG_EXTENDED);
     if (result)
     {
-        fprintf(stderr, RED "[Error]" RES "Could not compile regex\n");
+        fprintf(stderr, RED "[Error] " RES "Could not compile regex %s\n", pattern);
         return 0;
     }
 
     result = regexec(&regex, input, 0, NULL, 0);
     regfree(&regex);
     return result == 0; // 0 -> found match
+}
+
+bool isValidPortNumber(int port_number)
+{
+    return port_number >= 1 && port_number <= 65535;
 }
 
 int isSingleNumber(const char *input, int *port)
@@ -327,42 +211,146 @@ int isValidRange(const char *input, int *start, int *end)
     return isValidPortNumber(*start) && isValidPortNumber(*end) && (*start <= *end);
 }
 
-int test()
+// source: https://stackoverflow.com/a/17773849
+bool isValidUrl(const char *url)
 {
-    const char *inputs[] = {"22", "22,23,24", "1-65535", "22,80-82", "22,", "22-65536", "0-5", "65535-1", NULL};
+    return regmatch("^(https?://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}|www\\.[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})$", url) || strcmp("localhost", url) == 0;
+}
 
-    for (int i = 0; inputs[i] != NULL; i++)
+// source: https://stackoverflow.com/a/36760050
+bool isValidIpv4(const char *ipv4)
+{
+    return regmatch("^((25[0-5]|(2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]))\\.){3}(25[0-5]|(2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9]))$", ipv4);
+}
+
+// source: https://stackoverflow.com/a/17871737
+bool isValidIpv6(const char *ipv6)
+{
+    return regmatch("^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$", ipv6);
+}
+
+typedef struct Options
+{
+    char *interface;
+    char *udp_ports;
+    char *tcp_ports;
+    int timeout;
+    char *target;
+} Options;
+
+Options parse_options(int argc, char **argv)
+{
+    if (argc == 1)
     {
-        const char *input = inputs[i];
-        printf("Input: \"%s\"\n", input);
-
-        int port, ports[65536], count, start, end;
-
-        if (isSingleNumber(input, &port))
-        {
-            printf("Valid: Single number %d\n", port);
-        }
-        else if (isCommaSeparatedList(input, ports, &count))
-        {
-            printf("Valid: List of %d ports\n", count);
-            for (int i = 0; i < count; i++)
-            {
-                printf("    > %d\n", ports[i]);
-            }
-        }
-        else if (isValidRange(input, &start, &end))
-        {
-            printf("Valid: Range from %d to %d\n", start, end);
-        }
-        else
-        {
-            printf("Invalid\n");
-        }
-
-        printf("\n");
+        printNetworkInterfaces();
+        exit(EXIT_SUCCESS);
     }
 
-    return 0;
+    Options opts = {.interface = NULL, .udp_ports = NULL, .tcp_ports = NULL, .timeout = 5000, .target = NULL};
+
+    int opt;
+    const char *short_options = "i::u:t:w:";
+    static struct option long_options[] = {{"interface", optional_argument, 0, 'i'},
+                                           {"pu", required_argument, 0, 'u'},
+                                           {"pt", required_argument, 0, 't'},
+                                           {"timeout", required_argument, 0, 'w'},
+                                           {0, 0, 0, 0}};
+
+    while ((opt = getopt_long(argc, argv, short_options, long_options, NULL)) != -1)
+    {
+        switch (opt)
+        {
+        case 'i':
+            if (opts.interface != NULL)
+            {
+                fprintf(stderr, RED "[Error] " RES "Interface is already specified.\n");
+                exit(EXIT_FAILURE);
+            }
+
+            if (optarg)
+            {
+                opts.interface = optarg;
+            }
+            else if (optind < argc && argv[optind][0] != '-')
+            {
+                opts.interface = argv[optind++];
+            }
+            else
+            {
+                printNetworkInterfaces();
+                exit(EXIT_SUCCESS);
+            }
+
+            if (!isInterfaceValid(opts.interface))
+            {
+                fprintf(stderr, RED "[Error] " RES "Interface is not valid\n");
+                exit(EXIT_FAILURE);
+            }
+            break;
+        case 'u':
+            if (opts.udp_ports != NULL)
+            {
+                fprintf(stderr, RED "[Error] " RES "UDP port-ranges are already specified.\n");
+                exit(EXIT_FAILURE);
+            }
+
+            int uport, uports[65535], ucount, ustart, uend; // TODO use bitset for this 
+            if (!isSingleNumber(optarg, &uport) && !isCommaSeparatedList(optarg, uports, &ucount) && !isValidRange(optarg, &ustart, &uend))            
+            {
+                fprintf(stderr, RED "[Error] " RES "Invalid UDP port range\n");
+                exit(EXIT_FAILURE);
+            }
+
+            opts.udp_ports = optarg;
+            break;
+        case 't':
+            if (opts.tcp_ports != NULL)
+            {
+                fprintf(stderr, RED "[Error] " RES "TCP port-ranges are already specified.\n");
+                exit(EXIT_FAILURE);
+            }
+
+            int tport, tports[65535], tcount, tstart, tend; // TODO use bitset for this 
+            if (!isSingleNumber(optarg, &tport) && !isCommaSeparatedList(optarg, tports, &tcount) && !isValidRange(optarg, &tstart, &tend))            
+            {
+                fprintf(stderr, RED "[Error] " RES "Invalid TCP port range\n");
+                exit(EXIT_FAILURE);
+            }
+
+            opts.tcp_ports = optarg;
+            break;
+        case 'w': {
+            if (!regmatch("^[1-9][0-9]*$", optarg))
+            {
+                fprintf(stderr, RED "[Error] " RES "Invalid timeout value.\n");
+                exit(EXIT_FAILURE);
+            }
+            opts.timeout = atoi(optarg);
+            break;
+        }
+        case '?': {
+            exit(EXIT_FAILURE);
+        }
+        default:
+            fprintf(stderr, RED "[Error] " RES "Failed unexpectedly.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (optind != argc - 1)
+    {
+        fprintf(stderr, RED "[Error] " RES "Exactly one domain-name or IP-address must be provided.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    opts.target = argv[optind];
+    if (!isValidUrl(opts.target) && !isValidIpv4(opts.target) && !isValidIpv6(opts.target))
+    {
+        fprintf(stderr, RED "[Error] " RES "%s is not a valid target address.\n", opts.target);
+        exit(EXIT_FAILURE);
+    }
+
+    return opts;
 }
 
 // https://www.geeksforgeeks.org/creating-a-portscanner-in-c/
@@ -383,5 +371,5 @@ int main(int argc, char **argv)
 
     portScanner(opts);
 
-    return test();
+    return 0;
 }
